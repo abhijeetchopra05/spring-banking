605d1ab8b0f8738246de91ecd9c585c677ba60fb
abhijit.chopra@thesunflowerlab.com <abhijit.chopra@thesunflowerlab.com>
2019-10-18 12:56:53 +0530

    added custom user detail service for spring security

---
+++ src/main/java/com/personal/banking/config/CustomUserDetailService.java
@@ -0,0 +1,51 @@
+package com.personal.banking.config;
+
+import com.personal.banking.domain.Role;
+import com.personal.banking.domain.User;
+import com.personal.banking.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepo userRepo;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> userOptional = userRepo.findByUserName(username);
+
+        org.springframework.security.core.userdetails.User.UserBuilder userBuilder;
+
+        if (userOptional.isPresent()) {
+
+            User user = userOptional.get();
+            userBuilder = org.springframework.security.core.userdetails.User.withUsername(username);
+            userBuilder.password(passwordEncoder.encode(user.getPassword()));
+
+            List<Role> roleList = new ArrayList<>(user.getRoleSet());
+            String[] roles = new String[roleList.size()];
+
+            for (int i = 0; i < roleList.size() - 1; i++) {
+                roles[i] = roleList.get(i).getName();
+            }
+
+            userBuilder.authorities(roles);
+        } else {
+            throw new UsernameNotFoundException("User does not exist");
+        }
+
+        return userBuilder.build();
+    }
+}
